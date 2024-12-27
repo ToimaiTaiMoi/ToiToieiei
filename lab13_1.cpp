660b1c412bc1e0b342c690391471e100c0e2f1db
ToiMaiTaiMoi_Tum <iloveusomuchx2@gmail.com>
2024-12-27 16:48:10 +0700

    "lab13 completed"

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,45 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double sum = 0;
+    
+    for(int i = 0 ; i < N ; i++){
+        sum = sum + A[i];
+    }
+    B[0] = sum / N;
+
+    double sum1 = 0;
+    for(int i = 0 ; i < N ; i++){
+        sum1 = sum1 + pow(A[i] - B[0] , 2) / N;
+    }
+    B[1] = sqrt(sum1);
+
+    double sum2 = 1;
+    for(int i = 0 ; i < N ; i++){
+        sum2 = sum2 * A[i];
+    }
+    B[2] = pow(sum2, 1.0 / N);
+
+    double sum3 = 0;
+    for(int i = 0 ; i < N ; i++){
+        sum3 = sum3 + (1 / A[i]);
+    }
+    B[3] = N / sum3;
+    
+    double max = 0;
+    double min = A[0];
+    for(int i = 0 ; i < N ; i++){
+        if(A[i] > max){
+            max = A[i];
+        }
+        if(A[i] < min){
+            min = A[i];
+        }
+        
+    }
+    B[4] = max;
+    B[5] = min;
+
+    
+}
